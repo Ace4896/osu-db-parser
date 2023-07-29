623fa5ff4a4cfb4266404fccd02429b7c587f306
Jon Pacheco <jon.pacheco@tutanota.com>
2023-07-29 12:33:50 +0100

    feat: add prelude module

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod beatmaps;
 pub mod collections;
 pub mod common;
 pub mod error;
+pub mod prelude;
 pub mod scores;
---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+pub use {
+    crate::beatmaps::{BeatmapEntry, BeatmapListing, RankedStatus, TimingPoint},
+    crate::collections::{Collection, CollectionListing},
+    crate::common::{GameplayMode, Mods, OsuString},
+    crate::error::Error,
+    crate::scores::{BeatmapScores, ScoreListing, ScoreReplay},
+};
