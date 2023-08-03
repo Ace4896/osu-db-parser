26a68bc1bd17d3dc22fe3f3df55f3fcd96942009
Jon Pacheco <jon.pacheco@tutanota.com>
2023-08-03 14:28:07 +0100

    feat: skeleton ui for displaying open beatmap listings

--- viewer/src/app.rs
+++ viewer/src/app.rs
@@ -1,118 +1,226 @@
+use egui::Id;
 use osu_db_parser::prelude::*;
 
 use crate::widgets::file_dialog::FileDialog;
 
+/// Holds the state for the main application.
 pub struct MainApp {
+    next_id: u32,
     file_dialog: FileDialog,
-    waiting_for_db: Option<DatabaseType>,
+    pending_file_operation: Option<FileOperation>,
 
-    opened_dbs: Vec<OpenedDatabase>,
+    open_beatmap_listings: Vec<OpenBeatmapListing>,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum DatabaseType {
-    BeatmapListing,
-    CollectionListing,
-    ScoreListing,
+/// Represents a file operation requested by the user.
+#[derive(Clone, Copy, Debug)]
+pub enum FileOperation {
+    GetBeatmapListing,
+    GetCollectionListing { id: Id },
+    GetScoreListing { id: Id },
+    GetReplay { id: Id },
 }
 
-pub enum OpenedDatabase {
-    Beatmaps { name: String, db: BeatmapListing },
-    Collections { name: String, db: CollectionListing },
-    Scores { name: String, db: ScoreListing },
+/// Holds the state for any open beatmap listings.
+pub struct OpenBeatmapListing {
+    id: Id,
+    title: String,
+    visible: bool,
+
+    beatmap_listing: BeatmapListing,
+    collection_listings: Vec<CollectionListing>,
+    score_listings: Vec<ScoreListing>,
+    replays: Vec<ScoreReplay>,
 }
 
 impl Default for MainApp {
     fn default() -> Self {
         Self {
+            next_id: 0,
             file_dialog: FileDialog::default(),
-            waiting_for_db: None,
+            pending_file_operation: None,
 
-            opened_dbs: Vec::new(),
-        }
-    }
-}
-
-impl std::fmt::Display for DatabaseType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DatabaseType::BeatmapListing => write!(f, "Beatmap Listing"),
-            DatabaseType::CollectionListing => write!(f, "Collection Listing"),
-            DatabaseType::ScoreListing => write!(f, "Score Listing"),
+            open_beatmap_listings: Vec::new(),
         }
     }
 }
 
 impl eframe::App for MainApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.check_for_files();
-        self.menu_bar(ctx, frame);
+        self.side_panel(ctx);
+
+        self.beatmap_listing_windows(ctx);
     }
 }
 
 impl MainApp {
+    /// Sets up a new beatmap listing that was just opened.
+    fn setup_beatmap_listing(&mut self, beatmap_listing: BeatmapListing) -> OpenBeatmapListing {
+        let id = self.next_id;
+        let title = format!("Beatmap Listing #{}", id);
+
+        self.next_id += 1;
+
+        OpenBeatmapListing {
+            id: Id::new(id),
+            title,
+            visible: true,
+            beatmap_listing,
+            collection_listings: Vec::new(),
+            score_listings: Vec::new(),
+            replays: Vec::new(),
+        }
+    }
+
+    /// Finds the opened database with the specified ID.
+    fn find_opened_database(&mut self, id: Id) -> Option<&mut OpenBeatmapListing> {
+        self.open_beatmap_listings.iter_mut().find(|db| db.id == id)
+    }
+
     /// Checks if we are waiting for a file and attempts to parse it if it has been loaded.
     fn check_for_files(&mut self) {
-        if let Some(db_type) = self.waiting_for_db {
+        if let Some(file_operation) = self.pending_file_operation {
             if let Some(data) = self.file_dialog.get() {
-                let name = format!("#{} - {}", self.opened_dbs.len() + 1, db_type);
-
-                let opened_db = match db_type {
-                    DatabaseType::BeatmapListing => BeatmapListing::from_bytes(&data)
-                        .map(|db| OpenedDatabase::Beatmaps { name, db }),
-                    DatabaseType::CollectionListing => CollectionListing::from_bytes(&data)
-                        .map(|db| OpenedDatabase::Collections { name, db }),
-                    DatabaseType::ScoreListing => ScoreListing::from_bytes(&data)
-                        .map(|db| OpenedDatabase::Scores { name, db }),
-                };
-
-                match opened_db {
-                    Ok(db) => {
-                        log::debug!("Successfully opened {} ({} bytes)", db_type, data.len());
-                        self.opened_dbs.push(db);
+                match file_operation {
+                    FileOperation::GetBeatmapListing => match BeatmapListing::from_bytes(&data) {
+                        Ok(beatmap_listing) => {
+                            let opened_beatmap_listing =
+                                self.setup_beatmap_listing(beatmap_listing);
+                            self.open_beatmap_listings.push(opened_beatmap_listing);
+                        }
+                        Err(e) => log::warn!("Unable to open beatmap listing: {}", e),
+                    },
+                    FileOperation::GetCollectionListing {
+                        id: open_database_id,
+                    } => {
+                        if let Some(opened_database) = self.find_opened_database(open_database_id) {
+                            match CollectionListing::from_bytes(&data) {
+                                Ok(collection_listing) => {
+                                    opened_database.collection_listings.push(collection_listing)
+                                }
+                                Err(e) => log::warn!("Unable to open collection listing: {}", e),
+                            }
+                        }
                     }
-                    Err(e) => {
-                        log::warn!("Unable to open {}: {}", db_type, e);
+                    FileOperation::GetScoreListing {
+                        id: open_database_id,
+                    } => {
+                        if let Some(opened_database) = self.find_opened_database(open_database_id) {
+                            match ScoreListing::from_bytes(&data) {
+                                Ok(score_listing) => {
+                                    opened_database.score_listings.push(score_listing)
+                                }
+                                Err(e) => log::warn!("Unable to open score listing: {}", e),
+                            }
+                        }
+                    }
+                    FileOperation::GetReplay {
+                        id: open_database_id,
+                    } => {
+                        if let Some(opened_database) = self.find_opened_database(open_database_id) {
+                            match ScoreReplay::from_bytes(&data) {
+                                Ok(replay) => opened_database.replays.push(replay),
+                                Err(e) => log::warn!("Unable to open replay file: {}", e),
+                            }
+                        }
                     }
                 }
+
+                self.pending_file_operation = None;
             }
         }
     }
 
-    /// Renders the menu bar in the main window.
-    fn menu_bar(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            egui::menu::bar(ui, |ui| {
-                // File Menu
-                ui.menu_button("File", |ui| {
-                    if ui.button("Open osu.db...").clicked() {
-                        self.waiting_for_db = Some(DatabaseType::BeatmapListing);
-                        self.file_dialog.open();
-                        ui.close_menu();
-                    }
+    /// Renders the side bar which displays any opened beatmap listings.
+    fn side_panel(&mut self, ctx: &egui::Context) {
+        egui::SidePanel::left("left_panel").show(ctx, |ui| {
+            ui.vertical_centered(|ui| ui.heading("Open Databases"));
 
-                    if ui.button("Open collection.db...").clicked() {
-                        self.waiting_for_db = Some(DatabaseType::CollectionListing);
-                        self.file_dialog.open();
-                        ui.close_menu();
-                    }
+            if ui.button("Open osu.db...").clicked() {
+                if self.pending_file_operation.is_none() {
+                    self.pending_file_operation = Some(FileOperation::GetBeatmapListing);
+                    self.file_dialog.open();
+                }
+            }
 
-                    if ui.button("Open scores.db...").clicked() {
-                        self.waiting_for_db = Some(DatabaseType::ScoreListing);
-                        self.file_dialog.open();
-                        ui.close_menu();
-                    }
-
-                    #[cfg(not(target_arch = "wasm32"))]
-                    {
-                        ui.separator();
-
-                        if ui.button("Quit").clicked() {
-                            _frame.close();
+            for open_beatmap_listing in self.open_beatmap_listings.iter_mut() {
+                ui.collapsing(&open_beatmap_listing.title, |ui| {
+                    // Open the beatmap listing window (or focus it if it's already open)
+                    if ui.button("View Details").clicked() {
+                        if open_beatmap_listing.visible {
+                            ui.memory_mut(|memory| memory.request_focus(open_beatmap_listing.id));
+                        } else {
+                            open_beatmap_listing.visible = true;
                         }
                     }
-                })
-            })
+
+                    // Headers for collections, scores and replays
+                    ui.collapsing("Collection Listings", |ui| {
+                        if ui.button("Open collection.db...").clicked() {
+                            if self.pending_file_operation.is_none() {
+                                self.pending_file_operation =
+                                    Some(FileOperation::GetCollectionListing {
+                                        id: open_beatmap_listing.id,
+                                    });
+
+                                self.file_dialog.open();
+                            }
+                        }
+
+                        for (i, _) in open_beatmap_listing
+                            .collection_listings
+                            .iter_mut()
+                            .enumerate()
+                        {
+                            let _ = ui.button(&format!("Collection Listing #{}", i));
+                        }
+                    });
+
+                    ui.collapsing("Score Listings", |ui| {
+                        if ui.button("Open scores.db...").clicked() {
+                            if self.pending_file_operation.is_none() {
+                                self.pending_file_operation =
+                                    Some(FileOperation::GetScoreListing {
+                                        id: open_beatmap_listing.id,
+                                    });
+
+                                self.file_dialog.open();
+                            }
+                        }
+
+                        for (i, _) in open_beatmap_listing.score_listings.iter_mut().enumerate() {
+                            let _ = ui.button(&format!("Score Listing #{}", i));
+                        }
+                    });
+
+                    ui.collapsing("Replays", |ui| {
+                        if ui.button("Open .osr replay...").clicked() {
+                            if self.pending_file_operation.is_none() {
+                                self.pending_file_operation = Some(FileOperation::GetReplay {
+                                    id: open_beatmap_listing.id,
+                                });
+
+                                self.file_dialog.open();
+                            }
+                        }
+
+                        for (i, _) in open_beatmap_listing.replays.iter_mut().enumerate() {
+                            let _ = ui.button(&format!("Replay #{}", i));
+                        }
+                    });
+                });
+            }
         });
+    }
+
+    /// Renders the beatmap listing windows.
+    fn beatmap_listing_windows(&mut self, ctx: &egui::Context) {
+        for open_beatmap_listing in self.open_beatmap_listings.iter_mut().filter(|b| b.visible) {
+            egui::Window::new(&open_beatmap_listing.title)
+                .id(open_beatmap_listing.id)
+                .open(&mut open_beatmap_listing.visible)
+                .show(ctx, |ui| {});
+        }
     }
 }
